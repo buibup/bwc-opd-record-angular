47babdfbd1f9e19ee49df9aa82d941c1cadb14a4
buibup <b5209194@gmail.com>
2018-06-17 23:26:28 +0700

    show patient info

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { navItems } from './../../_nav';
+import { PatientService } from '../../services/patient.service';
+import { EpisodeService } from '../../services/episode.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +12,9 @@ export class DefaultLayoutComponent {
   public sidebarMinimized = true;
   private changes: MutationObserver;
   public element: HTMLElement = document.body;
-  constructor() {
+
+  constructor(public episodeService: EpisodeService,
+    public patientService: PatientService) {
 
     this.changes = new MutationObserver((mutations) => {
       this.sidebarMinimized = document.body.classList.contains('sidebar-minimized');
