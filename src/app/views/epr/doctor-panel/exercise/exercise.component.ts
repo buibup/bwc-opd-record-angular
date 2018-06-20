e1deaab49afda39a3e40c35b6a9b1b0be310628d
buibup <b5209194@gmail.com>
2018-06-20 13:22:00 +0700

    complete excercise component

--- src/app/views/epr/doctor-panel/exercise/exercise.component.ts
+++ src/app/views/epr/doctor-panel/exercise/exercise.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DoctorService } from '../../../../services/doctor.service';
 
 @Component({
   selector: 'app-exercise',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExerciseComponent implements OnInit {
 
-  constructor() { }
+  constructor(public doctorService: DoctorService) { }
 
   ngOnInit() {
+  }
+
+  isCollapsed: boolean = true;
+  isExercise: boolean = true;
+
+  collapsed(event: any): void {
+    // console.log(event);
+  }
+
+  expanded(event: any): void {
+    // console.log(event);
   }
 
 }
