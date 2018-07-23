7850eb81bcfd859f09e833afd8809773580cc7dc
buibup <b5209194@gmail.com>
2018-07-23 23:45:02 +0700

    preview pdf and jpg image

--- src/app/views/epr/scan-documents/scan-documents.component.ts
+++ src/app/views/epr/scan-documents/scan-documents.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScanDocumentsComponent implements OnInit {
 
+  dataLocalUrl = 'http://10.90.10.18/BwcOpdRecordApi/api/OpdRecord/GetDocument/81-17-000017/373';
+
   constructor() { }
 
   ngOnInit() {
